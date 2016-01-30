71b0e54257b23062bb0d1e9111f6d01854bd5428
Ryan Borek <ryanborek.ca@gmail.com>
2016-01-29 19:39:39 -0800

    initial commit

---
+++ desktop/src/game/ritual/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.ritual.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.ritual.RitualGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RitualGame(), config);
+	}
+}
