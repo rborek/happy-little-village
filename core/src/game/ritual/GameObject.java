ee5d62afdbd68dce01becd97e1bb534dd7a41506
Duke Nguyen <nguyentranduc1@gmail.com>
2016-01-29 20:10:49 -0800

    Create gameobject class

--- core/src/game/ritual/GameObject.java
+++ core/src/game/ritual/GameObject.java
@@ -1,6 +1,9 @@
 package game.ritual;
 
-public class GameObject {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class GameObject {
     /* TODO
      * member variables:
      * width
@@ -17,4 +20,36 @@ public class GameObject {
      *
      *
      */
+	protected float height;
+	protected Vector2 position;
+	protected Texture text;
+	protected float width;
+	
+	GameObject(Texture text, Vector2 position){
+		this.text=text;
+		this.position= position;
+	}
+	
+	public abstract void update(float delta);
+	
+	
+	public float getWidth() {
+		return width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public Vector2 getPosition() {
+		return position;
+	}
+
+	public Texture getText() {
+		return text;
+	}
+
+	
+	
+	
 }
