29ecbe92490c4c3501b0e127e04357f2a2b3d65f
Ryan Borek <ryanborek.ca@gmail.com>
2016-01-31 14:42:56 -0800

    book entry start

--- core/src/game/ritual/rituals/RitualBookEntry.java
+++ core/src/game/ritual/rituals/RitualBookEntry.java
@@ -1,4 +1,18 @@
 package game.ritual.rituals;
 
+import com.badlogic.gdx.graphics.Texture;
+import game.ritual.GameObject;
+
 public class RitualBookEntry extends GameObject {
+
+    protected RitualBookEntry(Texture texture, float xPos, float yPos) {
+        super(new Texture("scroll/sun.png"), xPos, yPos);
+        width = 300;
+        height = 300;
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
 }
